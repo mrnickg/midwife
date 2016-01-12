da8b2d935f3e0168afe696b080295e456c0c6ed1
Nick Goodall <nick.shimpo@gmail.com>
2016-01-12 16:44:20 +0000

    update js min

--- wordpress/wp-content/themes/listify-child/functions.php
+++ wordpress/wp-content/themes/listify-child/functions.php
@@ -18,7 +18,7 @@ function bh_enqueue_scripts() {
 	wp_enqueue_script( 'bh_profile', get_stylesheet_directory_uri() . "/js/profile.js", array( 'jquery' ) );
 
 	if (is_front_page()) {
-		wp_enqueue_script( 'bh_facets', get_stylesheet_directory_uri() . "/js/facetsupport.js", array( 'jquery' ) );
+		wp_enqueue_script( 'bh_facets', get_stylesheet_directory_uri() . "/js/facetsupport.js", array( 'jquery' ), '4.3.2' );
 	}
 }
 
